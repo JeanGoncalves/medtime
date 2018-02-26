899ca710fd6ea4232b5ea33e1395c56641d4173e
Jean Gonçalves <jean@htcomunicacao.com.br>
2018-02-26 12:40:51 -0300

    Implement dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,7 +7,8 @@ import { Enterprise } from '../../model/enterprise.model';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
